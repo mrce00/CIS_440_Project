1b979d8dca04e5237be0496089ab05bdca004e1d
mrce00 <mrice18@asu.edu>
2024-07-23 18:51:18 -0700

    fixed double window
    resolved issue that caused two webpages to open

--- app.py
+++ app.py
@@ -5,15 +5,14 @@ import webbrowser
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-
 @app.route('/button_clicked', methods=['POST'])
 def button_clicked():
-
     # Handle the button click event here
     mydb = mysql.connector.connect(
         host="107.180.1.16",
@@ -31,11 +30,12 @@ def button_clicked():
     for x in myresult:
         return (x)
 
+
 def open_browser():
     webbrowser.open_new('http://127.0.0.1:5000/')
+
 
 if __name__ == '__main__':
     # Start a timer to open the browser after Flask starts
     Timer(1, open_browser).start()
-    app.run(debug=True)
-
+    app.run(debug=False)
