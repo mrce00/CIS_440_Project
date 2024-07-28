596f510a005dca9f5b075cdd111f999ef3965c33
mrce00 <mrice18@asu.edu>
2024-07-28 09:46:26 -0700

    made into new py

--- app.py
+++ app.py
@@ -1,31 +1,3 @@
-import subprocess
-import sys
-
-import urllib.request
-
-def download_get_pip():
-    url = "https://bootstrap.pypa.io/get-pip.py"
-    filename = "get-pip.py"
-    with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
-        out_file.write(response.read())
-    return filename
-
-def install_pip():
-    get_pip_script = download_get_pip()
-    subprocess.check_call([sys.executable, get_pip_script])
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-# Install pip if not installed
-try:
-    import pip
-except ImportError:
-    install_pip()
-
-# Example: Install Flask-WTF
-install('Flask-WTF')
-
 import datetime
 
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
